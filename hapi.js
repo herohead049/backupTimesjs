794a9d21fc36bfe1a74379312b912335268dddcd
herohead049 <herohead@gmail.com>
2015-06-17 10:33:04 -0400

    running without redis

--- hapi.js
+++ hapi.js
@@ -1,4 +1,9 @@
 var Hapi = require('hapi');
+var cdlibjs = require('cdlibjs');
+var redis = require('redis');
+
+
+console.log(cdlibjs.getRedisAddress());
 
 var server = new Hapi.Server();
 server.connection({ port: 4000 });
@@ -7,18 +12,23 @@ fs = require('fs'),
     // NEVER use a Sync function except at start-up!
     index = fs.readFileSync(__dirname + '/index.html');
 
-
+// start of sockets
 
 var io = require('socket.io')(server.listener);
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
+    // Use socket to communicate with this particular client only, sending it it's own id
+    socket.emit('welcome', { message: 'Welcome!', id: socket.id });
+    //sendTime();
+    socket.on('i am client', console.log);
 
-    socket.emit('Oh hii!');
-
-    socket.on('burp', function () {
-        socket.emit('Excuse you!');
+    socket.on('button', function (data) {
+        console.log(data);
+        io.emit('button', data);
     });
 });
+
+// end of sockets
 
 server.route({
     method: 'GET',
@@ -36,9 +46,8 @@ server.route({
     }
 });
 
-//console.log(index);
 
-server.start();
+
 
 function sendTime(reqData) {
     if (reqData === '1') {
@@ -46,17 +55,6 @@ function sendTime(reqData) {
     } else {
         io.emit('client', { client: "us01s-epm04", description: "Hyperion Server", owner: "Deb David"});
     }
-    //io.emit('extra',{ time: "here is something else"});
 }
 
-// Send current time every 10 secs
-//setInterval(sendTime, 10000);
-//sendTime;
-// Emit welcome message on connection
-io.on('connection', function(socket) {
-    // Use socket to communicate with this particular client only, sending it it's own id
-    socket.emit('welcome', { message: 'Welcome!', id: socket.id });
-    //sendTime();
-    socket.on('i am client', console.log);
-});
-
+server.start();
